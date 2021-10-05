e793f6d205ca2ecf389b36521b3eb71428eb784c
vitorlinsbinski <69444717+vitorlinsbinski@users.noreply.github.com>
2021-10-05 19:54:34 -0400

    Add files via upload

---
+++ netflix-catalog.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 5,
+    slidesPerGroup: 4,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+  });
